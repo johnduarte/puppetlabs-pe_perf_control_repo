3eb7221a61da2eee60e6ec5e7d08336d1060d5d8
John Duarte <john.duarte@puppet.com>
2020-06-17 15:52:56 -0700

    fix quoting to quiet lint warnings

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,8 +28,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hi there!": }
-  notify { "Bonjour!": }
-  notify { "Hola!": }
-  notify { "Guten Tag!": }
+  notify { 'Hi there!': }
+  notify { 'Bonjour!': }
+  notify { 'Hola!': }
+  notify { 'Guten Tag!': }
 }
